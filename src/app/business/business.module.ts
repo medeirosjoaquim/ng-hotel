adb600a61bac4434fea0283db3e222dea26c8ffc
Joaquim Medeiros <joao.junior@br.experian.com>
2019-10-07 19:38:19 -0300

    board, checkin, consultas structure and styles

--- src/app/business/business.module.ts
+++ src/app/business/business.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board/board.component';
+import { CheckinComponent } from './checkin/checkin.component';
+import { ConsultasComponent } from './consultas/consultas.component';
 
 
 
 @NgModule({
-  declarations: [BoardComponent],
+  declarations: [BoardComponent, CheckinComponent, ConsultasComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BoardComponent
   ]
 })
 export class BusinessModule { }
