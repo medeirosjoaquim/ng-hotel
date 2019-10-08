224989b5a959de0d1a884b2d86b619eca5b3dace
Joaquim Medeiros <joao.junior@br.experian.com>
2019-10-08 19:28:41 -0300

    trap focus inside modal

--- src/app/business/pessoa/pessoa.component.ts
+++ src/app/business/pessoa/pessoa.component.ts
@@ -9,12 +9,13 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class PessoaComponent implements AfterViewInit {
   private element: any;
+  private firstInputElement: any;
+  private lastInputElement: any;
   private modalWindow: any;
   form: FormGroup;
 
   constructor(private el: ElementRef, private fb: FormBuilder) {
     this.element = el.nativeElement;
-
     this.form = this.fb.group({
       nome: ['', Validators.required],
       documento: ['', Validators.required],
@@ -25,7 +26,16 @@ export class PessoaComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.modalWindow = this.element.getElementsByTagName('div');
-
+    // trap the focus inside the modal
+    // get the first input
+    this.firstInputElement = this.element.
+    getElementsByClassName('first-input');
+    // get the last input
+    this.lastInputElement = this.element.getElementsByClassName('last-input');
+    // when last input loses focus, go back to the first one
+    this.lastInputElement[0].addEventListener('blur', () => {
+      this.firstInputElement[0].focus();
+    });
   }
     open(): void {
       this.modalWindow[0].classList.add('open');
