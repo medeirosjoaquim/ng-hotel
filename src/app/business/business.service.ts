5f7a97ad8069e0ea6a7a4721e4bc4bf721cb2d31
Joaquim Medeiros <joao.junior@br.experian.com>
2019-10-07 23:15:53 -0300

    use mock on service

---
+++ src/app/business/business.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BusinessService {
+
+  checkinsUrl = environment.checkinsUrl;
+  pessoasUrl = environment.pessoaUrl;
+  constructor(private http: HttpClient) { }
+
+  getCheckins(): Observable<any> {
+    return this.http.get<any>
+      (this.checkinsUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(err: any) {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
+  }
+}
