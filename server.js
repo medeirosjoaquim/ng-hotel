5f68234cb205eeb79877bf4dea699f05906ee798
Joaquim Medeiros <joao.junior@br.experian.com>
2019-10-09 00:16:42 -0300

    routes

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-//server.js
 const express = require('express');
-const favicon = require('express-favicon');
+const compression = require('compression')
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+
+// Serve static files....
+app.use(compression());
+app.use(express.static(__dirname + '/dist/hotel-senior'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/hotel-senior/index.html'));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(404).send('Something broke!')
+})
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
