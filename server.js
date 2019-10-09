c73afe377578fd553e5d36e8b275b62f2a7bf15f
Joaquim Medeiros <joao.junior@br.experian.com>
2019-10-08 23:41:14 -0300

    first commit for prod

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const compression = require('compression')
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(compression());
+app.use(express.static(__dirname + '/dist/hotel-senior'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/hotel-senior/index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(404).send('Something broke!')
+})
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
